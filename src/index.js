3eefb26f6c98a1fdb69cd38e36c28f77a906a1e0
yoonlight <yoonlight12@gmail.com>
2021-03-26 15:59:13 +0900

    complete apply mailservice to node

--- src/index.js
+++ src/index.js
@@ -1,77 +1,47 @@
 import amqplib from 'amqplib/callback_api'
 import dotenv from 'dotenv'
-
+import nodemailer from 'nodemailer'
 dotenv.config()
 
-const receive = (error0, connection) => {
+const mail = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  auth: {
+    user: process.env.EMAIL_ADDR,
+    pass: process.env.EMAIL_PWD,
+  },
+})
+
+amqplib.connect(process.env.RABBITMQ_URL, (error0, connection) => {
   if (error0) {
     throw error0
   }
-  connection.createChannel(function (error1, channel) {
-    if (error1) {
-      throw error1
-    }
-    var queue = 'hello'
-    channel.assertQueue(queue, {
-      durable: false,
-    })
-
-    console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', queue)
-
-    channel.consume(
-      queue,
-      function (msg) {
-        console.log(' [x] Received %s', msg.content.toString())
-      },
-      {
-        noAck: true,
-      }
-    )
-  })
-  connection.createChannel(function (error1, channel2) {
-    if (error1) {
-      throw error1
-    }
-    const queueTodo = 'todo'
-    channel2.assertQueue(queueTodo, {
-      durable: false,
-    })
-    console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', queueTodo)
-
-    channel2.consume(
-      queueTodo,
-      function (msg) {
-        console.log(' [x] Received %s', msg.content.toString())
-        var result = JSON.parse(msg.content.toString())
-        console.log(result)
-      },
-      {
-        noAck: true,
-      }
-    )
-  })
-  connection.createChannel(function (error1, channel3) {
+  connection.createChannel((error1, channel) => {
     if (error1) {
       throw error1
     }
     const queueMail = 'mail'
-    channel3.assertQueue(queueMail, {
-      durable: false,
-    })
-    console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', queueMail)
-
-    channel3.consume(
-      queueMail,
-      function (msg) {
-        console.log(' [x] Received %s', msg.content.toString())
-        var result = JSON.parse(msg.content.toString())
-        console.log(result)
-      },
-      {
-        noAck: true,
-      }
+    channel.assertQueue(queueMail, { durable: false })
+    console.log(
+      ' [*] Waiting for messages in %s. To exit press CTRL+C',
+      queueMail
     )
+    channel.consume(queueMail, function (msg) {
+      console.log(' [x] Received %s', msg.content.toString())
+      let result = JSON.parse(msg.content.toString())
+      console.log(result)
+      mail.sendMail(result, (error, info) => {
+        if (error) {
+          console.log(error)
+          console.error(error.stack)
+          return channel.nack(msg)
+        } else {
+          console.log('Email sent: ' + info.response)
+          console.log('Delivered message %s', info.messageId)
+          channel.ack(msg)
+        }
+      })
+    })
   })
-}
-
-amqplib.connect(process.env.RABBITMQ_URL, receive)
+})
